b980740dc2cf06afea0f33789f8a5445488b46ae
NguyenHuuTien2000 <tien.nguyenhuu.set18@eiu.edu.vn>
2023-07-12 00:22:33 +0700

    Implement Attendance to Events

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,13 @@
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public bool IsHost { get; set; }
+
+        public Activity Activity { get; set; }
+
+        public Guid ActivityId { get; set; }
+    }
+}
