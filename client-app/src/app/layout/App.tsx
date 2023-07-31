7328a672ee93e1cc12b23361c4c04cd80c289276
NguyenHuuTien2000 <tien.nguyenhuu.set18@eiu.edu.vn>
2023-07-31 23:26:17 +0700

    Build and config for production

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import { ToastContainer } from "react-toastify";
 import { useStore } from "../stores/store";
@@ -25,8 +25,8 @@ function App() {
   if (!commonStore.appLoaded) return <LoadingComponent content="Loading app..." />
 
   return (
-
     <Fragment>
+      <ScrollRestoration />
       <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
       <ModalContainer />
       {location.pathname === "/" ? <HomePage /> : (
