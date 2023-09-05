7ccc615d7d57d1b7b5e3129917061c37de7865cd
NguyenHuuTien2000 <tien.nguyenhuu.set18@eiu.edu.vn>
2023-09-05 14:29:20 +0700

    Fix creating events

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -46,7 +46,7 @@ export default observer(function ActivityForm() {
         ...activity,
         id: uuid()
       }
-      createActivity(newActivity).then(() => navigate(`/activities/${activity.id}`))
+      createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`))
     } else {
       updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
     }
